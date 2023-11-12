327139195e1b91df9ae40c1ef32d8525042a3d34
Karim-Ismail-Hassaneen <121400836+Karim-Ismail-Hassaneen@users.noreply.github.com>
2023-11-12 15:57:13 +0200

    Project is FINSHED

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
-import { useRef, useState } from 'react'
-import './App.css'
+import { useRef, useState } from "react";
+import "./App.css";
 
 function App() {
-  const [todos, setTodos] = useState([])
-  const handleInput = useRef()
+  const [todos, setTodos] = useState([]);
+  const handleInput = useRef();
 
   const handleInputValue = () => {
     let inputText = handleInput.current.value;
-    setTodos([...todos, inputText])
+    let newItem = { completed: false, inputText };
+    setTodos([...todos, newItem]);
     handleInput.current.value = " ";
+  };
+  const textDone = (index) => {
+    let newTodoList = todos.slice();
+    newTodoList[index].completed = !newTodoList[index].completed;
+    setTodos(newTodoList);
+  };
+  const textDelete = (index) => {
+    let newTodoList = [...todos];
+    newTodoList.splice(index, 1);
+    setTodos(newTodoList)
   }
   return (
     <>
@@ -17,18 +28,26 @@ function App() {
           <div className="app">
             <h2>Todo_List</h2>
             <ul>
-              {todos.map((item) => {
+              {todos.map(({ inputText, completed }, index) => {
                 // eslint-disable-next-line react/jsx-key
-                return <li>{item}</li>
+                return <div className="info">
+                  <li
+                   className={completed ? "done" : ""} 
+                   key={index} 
+                   onClick={() => textDone(index)}>
+                  {inputText}
+                  </li>
+                  <span onClick={() => textDelete(index)}>✖️</span>
+                </div>
               })}
             </ul>
-            <input ref={handleInput} placeholder='Please Enter Your Task' />
+            <input ref={handleInput} placeholder="Please Enter Your Task" />
             <button onClick={handleInputValue}>Add</button>
           </div>
         </div>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
